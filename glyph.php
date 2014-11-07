bbca3dd34d018ea1320554f5f1b5b2dbddab5a8d
John <john@jkms.me>
2014-11-07 15:24:33 -0800

    functions!

--- glyph.php
+++ glyph.php
@@ -3,25 +3,28 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 $string = "londondrugs";
-$domainstring = str_split($string);
 
-$glyphs['l'] = array("1", "I");
-$glyphs['o'] = array("0");
-$glyphs['n'] = array("ri");
-$glyphs['d'] = array("cl", "cI", "c1");
-$glyphs['r'] = NULL;
-$glyphs['u'] = NULL;
-$glyphs['g'] = array("q");
-$glyphs['s'] = array("z");
-
-$j = count($domainstring);
-for ($i=0; $i < $j; $i++) {
-	$l = count($glyphs[$domainstring[$i]]);
-	for ($k=0; $k < $l; $k++) {
-		$output = $domainstring;
-		$output[$i] = $glyphs[$domainstring[$i]][$k];
-		$returnoutput = implode('',$output);
-		echo "$returnoutput<br>\n";
+Function Glyph($input) {
+	$textarray = str_split($input);
+	$glyphs['l'] = array("1", "I");
+	$glyphs['o'] = array("0");
+	$glyphs['n'] = array("ri");
+	$glyphs['d'] = array("cl", "cI", "c1");
+	$glyphs['r'] = NULL;
+	$glyphs['u'] = NULL;
+	$glyphs['g'] = array("q");
+	$glyphs['s'] = array("z");
+	
+	$j = count($textarray);
+	for ($i=0; $i < $j; $i++) {
+		$l = count($glyphs[$textarray[$i]]);
+		for ($k=0; $k < $l; $k++) {
+			$output = $textarray;
+			$output[$i] = $glyphs[$textarray[$i]][$k];
+			$returnoutput = implode('',$output);
+			return $output;
+		}
 	}
 }
+echo glyph($string);
 ?>
