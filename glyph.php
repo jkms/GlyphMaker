4d6d622bc2930560207d06bbd91b3914469ede5a
John <john@jkms.me>
2014-11-07 15:50:25 -0800

    unglyphed testing

--- glyph.php
+++ glyph.php
@@ -5,8 +5,6 @@ error_reporting(E_ALL);
 $string = "londondrugs";
 
 Function glyph($input) {
-	$textarray = str_split($input);
-
 	$glyphs['l'] = array("1", "I");
 	$glyphs['o'] = array("0");
 	$glyphs['n'] = array("ri");
@@ -20,17 +18,23 @@ Function glyph($input) {
 	for ($i=0; $i<$j; $i++) {
 		$l = count($glyphs[$textarray[$i]]);
 		for ($k=0; $k<$l; $k++) {
-			$output=$textarray;
-			$output[$i]=$glyphs[$textarray[$i]][$k];
-			$test = implode('',$output);
-			echo "this is a test: $test<br>\n";
-			$returnoutput[] = implode('',$output);
+			if ($textarray[$i]['unglyphed']) {
+				$output=$textarray;
+				$output[$i]=$glyphs[$textarray[$i]][$k];
+				$output[$i]['unglyphed']=0;
+				$returnoutput[] = implode('',$output);
+			}
 		}
 	}
 	return $returnoutput;
 }
 
-$blahblah = glyph($string);
+$textarray = str_split($string);
+$j = count($textarray);
+for ($i=0; $i<$j; $i++) {
+	$textarray[$i]['unglyphed']=1;
+}
+$blahblah = glyph($textarray);
 print_r($blahblah);
 
 /*
