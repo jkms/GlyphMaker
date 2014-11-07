fe7f22c44a308cb9d53550b77edb1d0b4f42ebfb
John <john@jkms.me>
2014-11-07 15:00:39 -0800

    init

---
+++ glyph.php
@@ -0,0 +1,27 @@
+<?php
+
+$string = 'londondrugs';
+$domainstring = str_split($string);
+
+$glyphs['l'] = array("1", "I");
+$glyphs['o'] = array("0");
+$glyphs['n'] = array("ri");
+$glyphs['d'] = array("cl", "cI", "c1");
+// $glyphs['r'] = array("");
+// $glyphs['u'] = array("");
+$glyphs['g'] = array("q");
+$glyphs['s'] = array("z");
+
+// $j = count($domainstring);
+$j = 1;
+for ($i=0; $i < $j; $i++) {
+	$l = count($glyphs[$domainstring[$i]]);
+	for ($k=0; $k < $l; $k++) {
+		$output = $domainstring
+		$output[$i] = $glyphs[$domainstring[$i]];
+		$returnoutput = implode($output);
+		echo $returnoutput;
+	}
+}
+
+?>
